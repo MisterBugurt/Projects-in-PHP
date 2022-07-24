e90733bad07f94cb0b34cedcd3d34c4adc843718
Vladislav <107124878+MisterBugurt@users.noreply.github.com>
2022-07-24 17:19:19 +0300

    Add script

---
+++ First Script(Factorial search)/code.php
@@ -0,0 +1,45 @@
+<div style="font-size: 100px; margin-top:35%; text-align: center; font-family: sans-serif">
+
+<?php
+
+// Скрит для поиска факториала числа
+
+$num = 3; // Задаём число
+	
+$arr = [];
+$Factorial = 1;
+
+for($i = $num; $i < $num+1 and $i > 0; $i--) {
+	$arr[] = $i;
+}
+foreach($arr as $j) {
+	$Factorial *= $j; 
+}
+
+echo $Factorial; // Вывод результата
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
---
+++ First Script(Factorial search)/index.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<link href="scc.css" rel="stylesheet">
+
+<?php
+ error_reporting(E_ALL);
+ ini_set('display_errors', 'on');
+ 
+ include 'code.php';
+?>
