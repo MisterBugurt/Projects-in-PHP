919ba6a62583461eb03bcb54ed2316a62192e593
Vladislav <107124878+MisterBugurt@users.noreply.github.com>
2022-08-07 13:14:24 +0300

    Update sript(ver. 1.1).php

--- First Script(Factorial search)/sript(ver. 1.1).php
+++ First Script(Factorial search)/sript(ver. 1.1).php
@@ -1,6 +1,7 @@
 <div style="font-size: 100px; margin-top:35%; text-align: center; font-family: sans-serif">
 <?php
 
+// Скрит для поиска факториал числа после изучения функций ;)
 
 $num = 3; //задаём число
 
