f5c49273e4efabfb45f0fed7c4c4391d4f9f785c
Vladislav <107124878+MisterBugurt@users.noreply.github.com>
2022-08-07 13:13:21 +0300

    Update sript(ver. 1.1).php

--- First Script(Factorial search)/sript(ver. 1.1).php
+++ First Script(Factorial search)/sript(ver. 1.1).php
@@ -1,14 +1,13 @@
+<div style="font-size: 100px; margin-top:35%; text-align: center; font-family: sans-serif">
 <?php
 
 
-$num = 3;
+$num = 3; //задаём число
 
 $arr = range(1,$num);
 $res = array_product($arr);
-echo $res;
+
+echo $res; //вывод результата
 
 
-
-
-
-?>
+?></div>
